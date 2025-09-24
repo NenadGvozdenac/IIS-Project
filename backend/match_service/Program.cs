3b884efd4b9f6f37c47f075e9dc7c521eaa1cb49
Nemanja Vojnic <nemanja.vojnic@vega.local>
2025-09-24 11:47:02 +0200

    Local development

--- backend/match_service/Program.cs
+++ backend/match_service/Program.cs
@@ -24,7 +24,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseRouting();
 app.UseCors(corsPolicy);
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthentication();
 app.UseAuthorization();
 
